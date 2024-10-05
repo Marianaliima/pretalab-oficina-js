a3aad888c5c61cf9ee50bff6a65b633ded82b0f4
lili-ane <105889416+lili-ane@users.noreply.github.com>
2024-10-05 12:58:34 -0300

    Desafio final concluído

--- desafio-final/desafio-final/desafio.js
+++ desafio-final/desafio-final/desafio.js
@@ -8,3 +8,32 @@ Crie um programa que receba o número de medalhas de ouro, prata e bronze de um 
 #Argentina: 3 medalhas
 
 */
+
+const rankings = {};
+
+function calcularMedalhas() {
+    while (true) {
+        const pais = prompt("Digite o nome do país ou 'sair' para encerrar:");
+        if (pais.toLowerCase() === 'sair') {
+            break;
+        }
+
+        const ouro = parseInt(prompt(`Digite o número de medalhas de ouro de ${pais}:`)) || 0;
+        const prata = parseInt(prompt(`Digite o número de medalhas de prata de ${pais}:`)) || 0;
+        const bronze = parseInt(prompt(`Digite o número de medalhas de bronze de ${pais}:`)) || 0;
+
+        const totalMedalhas = ouro + prata + bronze;
+        rankings[pais] = totalMedalhas;
+    }
+
+    exibirRanking();
+}
+
+function exibirRanking() {
+    console.log("#Ranking de medalhas:");
+    for (const pais in rankings) {
+        console.log(`#${pais}: ${rankings[pais]} medalhas`);
+    }
+}
+
+calcularMedalhas();
