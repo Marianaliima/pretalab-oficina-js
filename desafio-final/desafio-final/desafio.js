98e8a0a24924c8884099d3461c34b0fa2d7591c1
marianaliima <marilimabrito13@gmail.com>
2024-10-05 12:10:19 -0300

    fix: adiciona novo desafio final

---
+++ desafio-final/desafio-final/desafio.js
@@ -0,0 +1,10 @@
+/* Contagem de Medalhas
+Crie um programa que receba o número de medalhas de ouro, prata e bronze de um país nas Olimpíadas e calcule o total de medalhas. Quando escrever 'sair', o programa deverá encerrar e mostrar na tela o ranking de medalhas no formato:
+
+
+#Ranking de medalhas:
+#Brasil: 7 medalhas
+#França: 6 medalhas
+#Argentina: 3 medalhas
+
+*/
